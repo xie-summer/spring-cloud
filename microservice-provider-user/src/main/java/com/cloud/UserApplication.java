8649207ec5049d9a1bed45a75f5132096f6865d8
sunder-xie <xieshengrong@live.com>
2017-09-02 23:34:44 +0800

    修复User模块确实eureka

--- microservice-provider-user/src/main/java/com/cloud/UserApplication.java
+++ microservice-provider-user/src/main/java/com/cloud/UserApplication.java
@@ -3,11 +3,13 @@ package com.cloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+//import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * 启动类
  */
 @SpringBootApplication
+//@EnableEurekaServer
 @EnableEurekaServer
 public class UserApplication {
     public static void main(String[] args) {
