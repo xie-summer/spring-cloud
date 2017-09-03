04ebc1c96c68facc7a45caa739dce7b4d61e9fd0
sunder-xie <xieshengrong@live.com>
2017-09-03 16:19:29 +0800

    更新

--- microservice-provider-user/src/main/java/com/cloud/core/AbstractService.java
+++ microservice-provider-user/src/main/java/com/cloud/core/AbstractService.java
@@ -7,6 +7,7 @@ import tk.mybatis.mapper.entity.Condition;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.util.List;
 
 /**
@@ -17,11 +18,20 @@ public abstract class AbstractService<T> implements IService<T> {
     @Autowired
     protected Mapper<T> mapper;
 
-    private Class<T> modelClass;    // 当前泛型真实类型的Class
-
+    private Class<T> clazz;    // 当前泛型真实类型的Class
+    private T model;           //当前泛型真实类型实体
     public AbstractService() {
-        ParameterizedType pt = (ParameterizedType) this.getClass().getGenericSuperclass();
-        modelClass = (Class<T>) pt.getActualTypeArguments()[0];
+        try {
+            Type genericSuperclass = getClass().getGenericSuperclass();
+            if (!(genericSuperclass instanceof ParameterizedType)) {
+                genericSuperclass = this.getClass().getSuperclass().getGenericSuperclass();
+            }
+            ParameterizedType parameterizedType = (ParameterizedType) genericSuperclass;
+            Class clazz = (Class) parameterizedType.getActualTypeArguments()[0];
+            model = (T) clazz.newInstance();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void save(T model) {
@@ -51,8 +61,8 @@ public abstract class AbstractService<T> implements IService<T> {
     @Override
     public T findBy(String property, Object value) throws TooManyResultsException {
         try {
-            T model = modelClass.newInstance();
-            Field field = modelClass.getDeclaredField(property);
+            T model = clazz.newInstance();
+            Field field = clazz.getDeclaredField(property);
             field.setAccessible(true);
             field.set(model, value);
             return mapper.selectOne(model);
