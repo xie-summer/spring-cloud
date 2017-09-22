f4ad07c956052aa82b4d668236e1055b597a86fb
sunder-xie <xieshengrong@live.com>
2017-09-22 17:36:47 +0800

    更新

--- microservice-provider-user/src/main/java/com/cloud/UserApplication.java
+++ microservice-provider-user/src/main/java/com/cloud/UserApplication.java
@@ -3,6 +3,7 @@ package com.cloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
 
@@ -10,6 +11,10 @@ import org.springframework.stereotype.Component;
  * 启动类
  */
 @SpringBootApplication
+/** 在classpath 下 两种格式是相同的
+ *  @EnableEurekaClient 更依赖于eureka注册中心
+ *  @EnableDiscoveryClient 可以在eureka中心或者zk，consul.所以更通用
+ *  */
 @EnableDiscoveryClient
 //@ComponentScan(basePackages="com.cloud")
 public class UserApplication {
